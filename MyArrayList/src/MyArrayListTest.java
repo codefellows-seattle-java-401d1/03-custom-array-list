d2b4775de6140f162fb3e1b78ca8d68d7d76fb10
GoldBeardSea <timbusch1@gmail.com>
2018-07-12 12:56:22 -0700

    forgot to fork

---
+++ MyArrayList/src/MyArrayListTest.java
@@ -0,0 +1,31 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyArrayListTest {
+    MyArrayList testArrayList = new MyArrayList(10);
+
+    @org.junit.jupiter.api.Test
+    public void addTest() {
+        testArrayList.add(3);
+//        assertArrayE
+    }
+
+    @org.junit.jupiter.api.Test
+    public void add1Test() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void removeValueTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void removeAtIndexTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void sizeTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void getTest() {
+    }
+}
