d669962a80c05716c15dfa94c9adc39fdd5b69a9
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-07-12 13:04:04 -0700

    Fix

---
+++ src/MyArrayListTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyArrayListTest {
+    MyArrayList TestArray = new MyArrayList();
+    @Test
+    void add() {
+    }
+}
